cc2d3728f2aba6d82e64b93c6a2efe5a2a2ffac1
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-16 15:39:43 +0530

    @GetExchange

--- src/main/java/com/microservice/dept/client/EmployeeClient.java
+++ src/main/java/com/microservice/dept/client/EmployeeClient.java
@@ -3,6 +3,7 @@ package com.microservice.dept.client;
 import com.microservice.dept.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.List;
 @HttpExchange
 public interface EmployeeClient {
 
-    @GetMapping("employee/dept/{dId}")
+    @GetExchange("employee/dept/{dId}")
     public List<Employee> findByDept(@PathVariable Long dId);
 }
