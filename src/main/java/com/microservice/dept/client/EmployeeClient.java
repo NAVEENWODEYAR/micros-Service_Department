08dca59af235dcd95f897c10e5a655ff8fda891d
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-16 16:03:11 +0530

    java.net.UnknownHostException: Failed to resolve 'NaveenWodeyar.mshome.net' [A(1)] after 2 queries

--- src/main/java/com/microservice/dept/client/EmployeeClient.java
+++ src/main/java/com/microservice/dept/client/EmployeeClient.java
@@ -1,7 +1,6 @@
 package com.microservice.dept.client;
 
 import com.microservice.dept.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
